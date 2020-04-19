dafad170261adfdee692b8746b3b012e858c17b5
Kamnetanker <31188231+kamnetanker@users.noreply.github.com>
2020-04-19 14:35:56 +0300

    v1.0.1
    Added include header file

---
+++ MemoryManagerIncludeFile.h
@@ -0,0 +1,15 @@
+#pragma once
+//ProjectHeaders
+#ifndef _MMG_
+#include "MemoryManager.h"
+#include "MemoryManager.cpp"
+#endif
+
+#ifndef _NODE_
+#include "Nodes.h"
+#endif
+
+#ifndef _LST_
+#include "Lst.h"
+#include "Lst.cpp"
+#endif
